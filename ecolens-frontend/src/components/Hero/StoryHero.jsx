db5248567fd601c98c97faf5b37a178cc13978c5
Anushka Singh <anushka019bteceai23@igdtuw.ac.in>
2025-05-23 18:27:11 +0530

    hero section updated

--- ecolens-frontend/src/components/Hero/StoryHero.jsx
+++ ecolens-frontend/src/components/Hero/StoryHero.jsx
@@ -138,7 +138,7 @@ const StoryHero = () => {
     <section id="home" className="relative bg-gradient-to-b from-[#e8f0e8] to-[#506850]/30 flex flex-col min-h-screen overflow-hidden">
       <Navbar />
       <div className="absolute inset-0 pointer-events-none">
-        {[...Array(100)].map((_, i) => (
+        {[...Array(150)].map((_, i) => (
           <svg
             key={i}
             className="absolute text-[#506850]/60"
